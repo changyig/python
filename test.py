7e2c9af7e46c4e2f8c5ea343aedad6771100e076
changyig <15716365368@163.com>
2019-08-11 22:53:21 +0800

    modified:   test.py

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 import xlrd
 import xlwt
+
+data = xlrd.open_workbook(r'C:\Users\Administrator\Desktop\tableExport.xlsx')
+table = data.sheets()[0] # 打开第一张表
+nrows = table.nrows # 获取表的行数
+ncols = table.ncols # 获取表的列数
+for i in range(nrows): # 循环逐行打印
+    if i == 0:# 跳过第一行
+        continue
+    print (table.row_values(i) )# 取前十三列
+
